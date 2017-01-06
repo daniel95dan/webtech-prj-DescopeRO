732b6dbf0ebae979ddbde76f5c813daf6892e935
Ionut Daniel Iancu <daniel_iancu96@yahoo.com>
2017-01-06 18:03:33 +0000

    v2.1

--- server.js
+++ server.js
@@ -1,26 +1,54 @@
 var express = require("express");
 //var bodyParser = require("body-parser");
 //var cors = require("cors");
-//var models  = require('./models/zona');
-//var Zona = models.zona;
 
 // init express application
 var app = express();
 //app.use(bodyParser.json());
 //app.use(cors());
+var Sequelize = require("sequelize");
 
-app.use(express.static('webtech-prj-descopero'));
+var sequelize = new Sequelize('descoperodb', 'daniel95dan', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+
+var zona = sequelize.define('Zona',{denumire: Sequelize.TEXT},{timestamps: false,freezeTableName: true});
+
+zona.findById(1).then(function(zone) {
+  console.log(zone)
+})
+
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
+
 app.get('/hello',function(req,res){
-   res.send("Hello World!");
+  zona.findById(1).then(function(zone) {
+  res.send(zone);
 })
+   
+})
+
 var nodeadmin=require('nodeadmin');
 app.use(nodeadmin(app));
 
-//app.use(require("./routes/zona.js"));
-//app.use('/descoperadb', express.static('descoperadb'));
 //app.use(express.static('app'));
 
 app.listen(process.env.PORT);
